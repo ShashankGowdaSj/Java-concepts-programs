6956ce11708d09bda3ca54a0d98afca6db382752
shashu <shashanksj05@gmail.com>
2026-03-12 17:05:19 +0530

    Add files via upload

---
+++ Day10/N_Queens.java
@@ -0,0 +1,60 @@
+package Day10;
+
+
+public class N_Queens {
+
+    static boolean isSafe(char[][] board, int row, int col) {
+
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void solve(char[][] board, int row) {
+
+        if (row == board.length) {
+            for (char[] r : board) {
+                System.out.println(java.util.Arrays.toString(r));
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                solve(board, row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        solve(board, 0);
+    }
+}
