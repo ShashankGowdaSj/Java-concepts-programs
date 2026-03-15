50a5b9b8efa5b360118d82013e77b3c88766e2a7
shashu <shashanksj05@gmail.com>
2026-03-15 20:40:18 +0530

    Add files via upload

---
+++ Day12/StaCk.java
@@ -0,0 +1,26 @@
+package Day12;
+
+import java.util.Stack;
+
+class StaCk {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if(c=='(' || c=='{' || c=='[') {
+                st.push(c);
+            }
+            else {
+                if(st.isEmpty()) return false;
+
+                char t = st.pop();
+
+                if(c==')' && t!='(') return false;
+                if(c=='}' && t!='{') return false;
+                if(c==']' && t!='[') return false;
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
